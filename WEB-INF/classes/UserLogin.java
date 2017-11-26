b8e77a7b8c275de64c5b58c946c661f593e66b03
Sujan Limbu <sujanlimbu8@gmail.com>
2017-11-27 00:32:56 +0545

    UserLogin.java fixed

--- WEB-INF/classes/UserLogin.java
+++ WEB-INF/classes/UserLogin.java
@@ -14,10 +14,14 @@ public class UserLogin extends HttpServlet{
 		try{
 			HttpSession session = request.getSession();
 			Connection cn = MySqlConnector.getConnection();
-			Statement stat = cn.createStatement();
+			Statement stat1 = cn.createStatement();
+			Statement stat2 = cn.createStatement();
 			ResultSet rsUser, rsAdmin;
 			RequestDispatcher rd;
-			rsUser = stat.executeQuery(userQuery);
+			rsUser = stat1.executeQuery(userQuery);
+			rsAdmin = stat2.executeQuery(adminQuery);	
+
+			/*
 			//checks if the normal user has logged in
 			while(rsUser.next()){
 				if(userName.equals(rsUser.getString("UserName")) && password.equals(rsUser.getString("Password"))){
@@ -33,7 +37,6 @@ public class UserLogin extends HttpServlet{
 			}
 		
 
-			rsAdmin = stat.executeQuery(adminQuery);	
 			//checks if the admin has logged in
 			while(rsAdmin.next()){
 				if(userName.equals(rsAdmin.getString("UserName")) && password.equals(rsAdmin.getString("Password"))){
@@ -46,6 +49,20 @@ public class UserLogin extends HttpServlet{
 				else{
 					out.println("<h3>Login UnSuccessfull. Either Username or Password is invalid. <a href = "+"./Index.html>Please try again</a>.</h3>");
 				}
+			}
+			*/
+			if(rsUser.next()){
+				rd = request.getRequestDispatcher("./Home.jsp");
+				request.setAttribute("uName",userName);
+				rd.include(request , response);
+			}
+			else if(rsAdmin.next()){
+				rd = request.getRequestDispatcher("./AdminHome.jsp");
+				request.setAttribute("uName",userName);
+				rd.include(request , response);
+			}
+			else{
+				out.println("<h3>Login UnSuccessfull. Either Username or Password is invalid. <a href = "+"./Index.html>Please try again</a>.</h3>");		
 			}	
 		} catch(Exception e){
 			out.println("Internal Error!!"+e.getMessage());
